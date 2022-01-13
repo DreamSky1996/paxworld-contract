1e47bdc5ac25eedcb3d5e10015d2458df15f1891
DreamSky1996 <webhipe@gmail.com>
2022-01-14 01:04:24 +0900

    test for update code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,14 +4,14 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account: " + deployer.address);
 
-  const PaxWorld = await ethers.getContractFactory("PaxWorld");
-  const paxWorld = await upgrades.deployProxy(PaxWorld, [42]);
-  await paxWorld.deployed();
+  // const PaxWorld = await ethers.getContractFactory("PaxWorld");
+  // const paxWorld = await upgrades.deployProxy(PaxWorld, [42]);
+  // await paxWorld.deployed();
   // Upgrading
-  // const PaxWorldV2 = await ethers.getContractFactory("PaxWorldV2");
-  // const upgraded = await upgrades.upgradeProxy(paxWorld.address, PaxWorldV2);
+  const PaxWorldV2 = await ethers.getContractFactory("PaxWorldV2");
+  const upgraded = await upgrades.upgradeProxy(paxWorld.address, PaxWorldV2);
 
-  console.log("PaxWorld deployed to:", paxWorld.address);
+  console.log("PaxWorld deployed to:", upgraded.address);
 }
 
 main()
